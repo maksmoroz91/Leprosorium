0aab9f41191a4a0407e885c05f60b85548bb6757
maksmoroz91 <dlkontakta7@gmail.com>
2020-10-19 15:53:30 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -4,17 +4,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 require "sqlite3"
 
+# функция которая создает глобальную базу... и она во 2-ой строке возвращается в хеш
 def init_db
 	@db = SQLite3::Database.new 'leprosorium.db'
 	@db.results_as_hash = true
 end	
 
+# вызывается каждый раз при перезагрузке любой страницы (не исполняется для CONFIGURE DO)
 before do
 	init_db 
 end
 
+# вызывается каждый раз при конфигурации приложения (изменился код или перезагрузилась страница)
 configure do
+	# инициализация БД
 	init_db
+
+	# создает таблицу если таблицы не существует
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts 
 		(
    			 id           INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,11 +33,14 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+#обработчик get-запроса /new (браузер получает страницу на сервер)
 get '/new' do
   	erb :new
 end
 
+#обработчик post-запроса /new (браузер отправляет данные на сервер)
 post '/new' do
+	#получаем переменную из post-запроса
 	content = params[:content]
 
 	if content.length <= 0
@@ -39,6 +48,7 @@ post '/new' do
 		return erb :new 
 	end 
 
+	#сохранение данных в БД
 	@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 
 	erb "You typed #{content}"
