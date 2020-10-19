830b60bf079f9313cfd44368f4a31d14afd917ed
maksmoroz91 <dlkontakta7@gmail.com>
2020-10-19 18:49:36 +0300

    Add details.erb view

--- app.rb
+++ app.rb
@@ -60,8 +60,11 @@ end
 
 # вывод информации о посте
 
-get '/posts/:post_id' do
+get '/details/:post_id' do
 	post_id = params[:post_id]
 
-	erb "Информация поста с ай-ди #{post_id}"
+	results = @db.execute 'select * from Posts where id = ?', [post_id]
+	@row = results[0]
+
+	erb :details
 end	
